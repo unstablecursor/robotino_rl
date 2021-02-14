f448d9ac950b6e22b2717571456a928ebe53ae24
zengerleTo <tobi.zengerle@gmail.com>
2021-02-14 06:05:03 +0100

    changed loading filepaths back to None

--- src/parameters/global_parameters.py
+++ src/parameters/global_parameters.py
@@ -10,8 +10,8 @@ class global_parameters:
 
     #network checkpoint save settings
     SAVE_EVERY_NTH_EPISODE = 10
-    policy_checkpoint_path = "/home/tobi/checkpoints/cleanup_test/policy_net_10.pth"# "/home/deniz/praktikum_final/catkin_ws/src/robotino_rl/models/policy_net_10.pth"
-    target_checkpoint_path = "/home/tobi/checkpoints/cleanup_test/target_net_10.pth"# "/home/deniz/praktikum_final/catkin_ws/src/robotino_rl/models/target_net_10.pth"
+    policy_checkpoint_path = None# "/home/deniz/praktikum_final/catkin_ws/src/robotino_rl/models/policy_net_10.pth"
+    target_checkpoint_path = None# "/home/deniz/praktikum_final/catkin_ws/src/robotino_rl/models/target_net_10.pth"
     checkpoint_root = "/home/tobi/checkpoints/cleanup_test"#"/home/deniz/praktikum_final/catkin_ws/src/robotino_rl/models"
 
     #path for pretrained model used for test run
